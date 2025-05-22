3a9ff518c2bd659c8dda7dca4c5a3d10bb4e1e2f
zylin1998 <jor13579@gmail.com>
2025-05-22 17:23:05 +0800

    興曾XInputDonet仿Unity輸入。

---
+++ Assets/Plugins/Loyufei.InputSystem/Core/Interface/IFakeKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loyufei.InputSystem
+{
+    internal interface IFakeKey
+    {
+        public bool GetKeyDown(int inputIndex);
+        public bool GetKey(int inputIndex);
+        public bool GetKeyUp(int inputIndex);
+    }
+}
---
+++ Assets/Plugins/Loyufei.InputSystem/Core/Internal/XInputDonetFakeInput.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using XInputDotNetPure;
+
+namespace Loyufei.InputSystem
+{
+    internal class XInputDonetFakeInput : IFakeKey
+    {
+        private int _HoldFrame    = 0;
+        private int _ReleaseFrame = 0;
+
+        public bool GetKeyDown(int inputIndex)
+        {
+            if (_HoldFrame == 0) 
+            {
+                _HoldFrame = GetValue(inputIndex) ? Time.frameCount : 0;
+            }
+
+            return _HoldFrame == Time.frameCount;
+        }
+
+        public bool GetKey(int inputIndex)
+        {
+            return GetValue(inputIndex) && _HoldFrame != Time.frameCount;
+        }
+
+        public bool GetKeyUp(int inputIndex)
+        {
+            if (_HoldFrame != 0 && !GetValue(inputIndex)) 
+            {
+                _HoldFrame    = 0;
+                _ReleaseFrame = Time.frameCount;
+            }
+
+            return _ReleaseFrame == Time.frameCount;
+        }
+
+        protected virtual bool GetValue(int index) 
+        {
+            return false;
+        }
+
+        private sealed class JoyStickA : XInputDonetFakeInput 
+        {
+            protected override bool GetValue(int index)
+            {
+                var playerIndex = (PlayerIndex)index - 1;
+                var state       = GamePad.GetState(playerIndex);
+
+                return state.Buttons.A == ButtonState.Pressed;
+            }
+        }
+
+        private sealed class JoyStickB : XInputDonetFakeInput
+        {
+            protected override bool GetValue(int index)
+            {
+                var playerIndex = (PlayerIndex)index - 1;
+                var state = GamePad.GetState(playerIndex);
+
+                return state.Buttons.B == ButtonState.Pressed;
+            }
+        }
+
+        private sealed class JoyStickX : XInputDonetFakeInput
+        {
+            protected override bool GetValue(int index)
+            {
+                var playerIndex = (PlayerIndex)index - 1;
+                var state = GamePad.GetState(playerIndex);
+
+                return state.Buttons.X == ButtonState.Pressed;
+            }
+        }
+
+        private sealed class JoyStickY : XInputDonetFakeInput
+        {
+            protected override bool GetValue(int index)
+            {
+                var playerIndex = (PlayerIndex)index - 1;
+                var state = GamePad.GetState(playerIndex);
+
+                return state.Buttons.Y == ButtonState.Pressed;
+            }
+        }
+
+        private sealed class JoyStickBack : XInputDonetFakeInput
+        {
+            protected override bool GetValue(int index)
+            {
+                var playerIndex = (PlayerIndex)index - 1;
+                var state = GamePad.GetState(playerIndex);
+
+                return state.Buttons.Back == ButtonState.Pressed;
+            }
+        }
+
+        private sealed class JoyStickStart : XInputDonetFakeInput
+        {
+            protected override bool GetValue(int index)
+            {
+                var playerIndex = (PlayerIndex)index - 1;
+                var state = GamePad.GetState(playerIndex);
+
+                return state.Buttons.Start == ButtonState.Pressed;
+            }
+        }
+    }
+}
